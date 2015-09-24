327f1b15d960e931d074133e4f9fbd1e53acc085
Blas Fernández Segura <burasu@gmail.com>
2015-09-24 19:03:20 +0200

    Implementamos plantilla para el tutorial

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,26 +1,34 @@
 'use strict';
 
-var app = angular.module('leafletAJS', [
-    'ngRoute',
-    'leaflet-directive',
-    'leafletAJS.controllers',
-    'leafletAJS.services'
-]);
+var app = angular.module('app', []);
 
-app.config(function($routeProvider) {
+function SeguroController($scope)
+{
+    $scope.seguro = {
+        nif: '',
+        nombre: '',
+        ape1: '',
+        edad: undefined,
+        sexo: '',
+        casado: false,
+        numHijos: undefined,
+        embarazada: false,
+        coberturas: {
+            oftalmologia: false,
+            dental: false,
+            fecundacionInVitro: false
+        },
+        enfermedades: {
+            corazon: false,
+            estomacal: false,
+            rinyones: false,
+            alergia: false,
+            nombreAlergia: ''
+        },
+        fechaCreacion: new Date()
+    }
+};
 
-    $routeProvider
-        .when('/', {
-            controller: 'MainCtrl',
-            templateUrl: 'views/main.tpl.html'
-        })
-
-        .when('/leaflet', {
-            controller: 'MainCtrl',
-            templateUrl: 'views/main.tpl.html'
-        })
-
-        .otherwise({
-            redirectTo: '/'
-        });
-});
+angular
+    .module('app')
+    .controller('SeguroController', SeguroController);
